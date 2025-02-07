ae0c2975747018e8a1f69b7e9959edd851c81add
Jeevan4n <jeevanskumar444@gmail.com>
2025-02-07 05:35:45 +0530

    modified in settings

--- yolo_pose_project/settings.py
+++ yolo_pose_project/settings.py
@@ -26,7 +26,8 @@ SECRET_KEY = os.environ.get('SECRET_KEY', 'your-default-secret-key')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = ['computer-vision-01.onrender.com']
+
 
 
 
